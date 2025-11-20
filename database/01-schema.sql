9f66d2c1b2bdb975911f0d9a2c3cecb86c179796
Juan <juanjoseblas344@gmail.com>
2025-11-19 21:37:57 -0500

    refactor(database): moves 'base_de_datos/' into the new 'database/' directory, separating the schema and initial data

---
+++ database/01-schema.sql
@@ -0,0 +1,154 @@
+CREATE DATABASE IF NOT EXISTS movixpress;
+USE movixpress;
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+/*!40101 SET NAMES utf8mb4 */;
+
+-- =======================
+-- Tabla: moneda
+-- =======================
+CREATE TABLE `moneda` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `simbolo` varchar(10) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `estado` int(11) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- =======================
+-- Tabla: documentos
+-- =======================
+CREATE TABLE `documentos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `documento` varchar(20) NOT NULL,
+  `estado` int(11) NOT NULL DEFAULT 1,
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- =======================
+-- Tabla: marcas
+-- =======================
+CREATE TABLE `marcas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `marca` varchar(50) NOT NULL,
+  `estado` int(11) NOT NULL DEFAULT 1,
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+-- =======================
+-- Tabla: tipos
+-- =======================
+CREATE TABLE `tipos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tipo` varchar(50) NOT NULL,
+  `estado` int(11) NOT NULL DEFAULT 1,
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- =======================
+-- Tabla: clientes
+-- =======================
+CREATE TABLE `clientes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `dni` varchar(10) DEFAULT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `telefono` varchar(15) NOT NULL,
+  `direccion` text NOT NULL,
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `estado` int(11) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- =======================
+-- Tabla: usuarios
+-- =======================
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `usuario` varchar(20) NOT NULL UNIQUE,
+  `nombre` varchar(100) NOT NULL,
+  `apellido` varchar(100) DEFAULT NULL,
+  `correo` varchar(80) NOT NULL,
+  `telefono` varchar(20) DEFAULT NULL,
+  `direccion` varchar(100) DEFAULT NULL,
+  `perfil` varchar(500) NOT NULL DEFAULT 'https://res.cloudinary.com/drwtssrej/image/upload/v1763577013/AlquilerAutos%28MovilXpress%29/default-user_zvjlu5.png',
+  `clave` varchar(100) NOT NULL,
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `estado` int(11) NOT NULL DEFAULT 1,
+  `rol` ENUM('ADMIN', 'WORKER') NOT NULL DEFAULT 'WORKER',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- =======================
+-- Tabla: configuracion
+-- =======================
+CREATE TABLE `configuracion` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ruc` varchar(20) NOT NULL,
+  `nombre` varchar(200) NOT NULL,
+  `telefono` varchar(15) NOT NULL,
+  `correo` varchar(100) NOT NULL,
+  `direccion` varchar(200) NOT NULL,
+  `mensaje` text NOT NULL,
+  `logo` varchar(10) NOT NULL,
+  `moneda` int(11) NOT NULL,
+  `impuesto` int(11) NOT NULL,
+  `cant_factura` int(11) NOT NULL,
+  `penalidad_por_dia` decimal(10,2) DEFAULT 0,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`moneda`) REFERENCES `moneda` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- =======================
+-- Tabla: vehiculos
+-- =======================
+CREATE TABLE `vehiculos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `placa` varchar(50) NOT NULL,
+  `id_marca` int(11) NOT NULL,
+  `id_tipo` int(11) NOT NULL,
+  `modelo` varchar(50) NOT NULL,
+  `foto` varchar(100) NOT NULL,
+  `actividad` ENUM('PRESTADO', 'LIBRE') NOT NULL DEFAULT 'LIBRE',
+  `estado` int(11) NOT NULL DEFAULT 1,
+  `precio_por_dia` decimal(10,2) DEFAULT 1,
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`id_marca`) REFERENCES `marcas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (`id_tipo`) REFERENCES `tipos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- =======================
+-- Tabla: alquiler
+-- =======================
+CREATE TABLE `alquiler` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_cliente` int(11) NOT NULL,
+  `id_vehiculo` int(11) NOT NULL,
+  `id_moneda` int(11) NOT NULL,
+  `num_dias` int(11) default 0,
+  `precio_dia` decimal(10,2) NOT NULL,
+  `abono` decimal(10,2) default 0,
+  `fecha_prestamo` date NOT NULL,
+  `hora` time NOT NULL,
+  `fecha_estimada_devolucion` date NOT NULL,
+  `fecha_real_devolucion` date default NULL,
+  `id_doc` int(11) NOT NULL,
+  `observacion` text DEFAULT NULL,
+  `estado`  ENUM('EN PRESTAMO', 'DEVUELTO','CANCELADO') NOT NULL DEFAULT 'EN PRESTAMO',
+  `penalidad` decimal(10,2) DEFAULT 0,
+  `penalidad_por_dia` decimal(10,2) DEFAULT 0,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`id_cliente`) REFERENCES `clientes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (`id_vehiculo`) REFERENCES `vehiculos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (`id_doc`) REFERENCES `documentos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (`id_moneda`) REFERENCES `moneda` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+COMMIT;
